28363ca6b1d9589ec750e28eeecc985ddee51021
Olga Riabenko <me@oriabenko.ru>
2025-01-06 15:06:54 +0300

    Add allure steps

--- src/test/java/com/demoqa/tests/RegistrationTest.java
+++ src/test/java/com/demoqa/tests/RegistrationTest.java
@@ -1,82 +1,126 @@
 package com.demoqa.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.RegistrationPage;
 import com.demoqa.utils.RegistrationData;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
+@DisplayName("Проверка формы регистрации")
 public class RegistrationTest extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     RegistrationData data = new RegistrationData();
 
     @Test
+    @DisplayName("Заполнение валидными данными всех полей")
     void fillAllFieldsWithValidDataTest() {
 
-        registrationPage.openPage()
-                .removeBlockingElements()
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.email)
-                .setGender(data.gender)
-                .setNumber(data.phone)
-                .setDateOfBirth(data.birthDay, data.birthMonth, data.birthYear)
-                .setSubjects(data.subjects)
-                .setHobbies(data.hobbies)
-                .setImagePath(data.image)
-                .setAddress(data.address)
-                .setState(data.state)
-                .setCity(data.city)
-                .submit();
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        registrationPage
-                .submissionCheckSuccess()
-                .submissionModalMessage("Thanks for submitting the form");
+        step("Открыть страницу с формой регистрации", () -> {
+            registrationPage.openPage()
+                    .removeBlockingElements();
+        });
 
-        registrationPage
-                .checkResultTablePairs("Student Name", data.firstName + " " + data.lastName)
-                .checkResultTablePairs("Student Email", data.email)
-                .checkResultTablePairs("Gender", data.gender)
-                .checkResultTablePairs("Mobile", data.phone)
-                .checkResultTablePairs("Date of Birth", data.birthDay + " " + data.birthMonth + "," + data.birthYear)
-                .checkResultTablePairs("Subjects", data.subjects)
-                .checkResultTablePairs("Hobbies", data.hobbies)
-                .checkResultTablePairs("Picture", data.image)
-                .checkResultTablePairs("Address", data.address)
-                .checkResultTablePairs("State and City", data.state + " " + data.city);
+        step("Заполнить все поля формы", () -> {
+            registrationPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.email)
+                    .setGender(data.gender)
+                    .setNumber(data.phone)
+                    .setDateOfBirth(data.birthDay, data.birthMonth, data.birthYear)
+                    .setSubjects(data.subjects)
+                    .setHobbies(data.hobbies)
+                    .setImagePath(data.image)
+                    .setAddress(data.address)
+                    .setState(data.state)
+                    .setCity(data.city);
+        });
+
+        step("Отправить форму", () -> {
+            registrationPage.submit();
+        });
+
+        step("Проверить, что форма была отправлена", () -> {
+            registrationPage
+                    .submissionCheckSuccess()
+                    .submissionModalMessage("Thanks for submitting the form");
+        });
+
+        step("Проверить результат отправки формы", () -> {
+            registrationPage
+                    .checkResultTablePairs("Student Name", data.firstName + " " + data.lastName)
+                    .checkResultTablePairs("Student Email", data.email)
+                    .checkResultTablePairs("Gender", data.gender)
+                    .checkResultTablePairs("Mobile", data.phone)
+                    .checkResultTablePairs("Date of Birth", data.birthDay + " " + data.birthMonth + "," + data.birthYear)
+                    .checkResultTablePairs("Subjects", data.subjects)
+                    .checkResultTablePairs("Hobbies", data.hobbies)
+                    .checkResultTablePairs("Picture", data.image)
+                    .checkResultTablePairs("Address", data.address)
+                    .checkResultTablePairs("State and City", data.state + " " + data.city);
+        });
     }
 
     @Test
+    @DisplayName("Заполнение валидными данными обязательных полей")
     void fillRequiredFieldsWithValidDataTest() {
-        registrationPage
-                .openPage()
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setNumber(data.phone)
-                .submit();
+        step("Открыть страницу с формой регистрации", () -> {
+            registrationPage.openPage()
+                    .removeBlockingElements();
+        });
 
-        registrationPage
-                .submissionCheckSuccess()
-                .submissionModalMessage("Thanks for submitting the form");
+        step("Заполнить данными обязательные поля формы регистрации", () -> {
+            registrationPage
+                    .setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setNumber(data.phone);
+        });
 
-        registrationPage
-                .checkResultTablePairs("Student Name", data.firstName + " " + data.lastName)
-                .checkResultTablePairs("Gender", data.gender)
-                .checkResultTablePairs("Mobile", data.phone);
+        step("Отправить форму", () -> {
+            registrationPage.submit();
+        });
+
+        step("Проверить, что форма была отправлена", () -> {
+            registrationPage
+                    .submissionCheckSuccess()
+                    .submissionModalMessage("Thanks for submitting the form");
+        });
+
+        step("Проверить результат отправки формы", () -> {
+            registrationPage
+                    .checkResultTablePairs("Student Name", data.firstName + " " + data.lastName)
+                    .checkResultTablePairs("Gender", data.gender)
+                    .checkResultTablePairs("Mobile", data.phone);
+        });
     }
 
     @Test
+    @DisplayName("Заполнение обязательных полей формы с невалидным номером телефона (9 символов)")
     void fillRequiredFieldsWithShortPhoneNumberNegativeTest() {
-        registrationPage
-                .openPage()
-                .setFirstName(data.firstName)
-                .setLastName(data.firstName)
-                .setGender(data.gender)
-                .setNumber(data.shortPhoneInvalid)
-                .submit();
+        step("Открыть страницу с формой регистрации", () -> {
+            registrationPage.openPage()
+                    .removeBlockingElements();
+        });
+        step("Заполнить данными обязательные поля формы регистрации с невалидным номером телефона (9 символов)", () -> {
+            registrationPage.setFirstName(data.firstName)
+                    .setLastName(data.firstName)
+                    .setGender(data.gender)
+                    .setNumber(data.shortPhoneInvalid);
+        });
 
-        registrationPage
-                .submissionCheckFail();
+        step("Отправить форму", () -> {
+            registrationPage.submit();
+        });
+        step("Проверить, что сообщение с уведомлением об успешной отправке не появилось", () -> {
+            registrationPage
+                    .submissionCheckFail();
+        });
 
     }
 }
